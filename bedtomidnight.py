0c1fffd544a74b6450300fb8af490e98354458cb
Ethan Dixius <ethan.dixius@gmail.com>
2019-09-04 21:20:33 -0400

    Testing BeforeBed and adding classes

---
+++ bedtomidnight.py
@@ -0,0 +1,14 @@
+from period import Period
+
+
+
+class BedToMidnight(Period):
+
+    def __init__(self, start=5, end=12):
+        assert(start >= 5)
+        self.start = start
+
+        assert(end >= 6)
+        self.end = end
+
+        self.rate = 8
